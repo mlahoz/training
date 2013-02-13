939f0a1e31a9e34907ac0de258a3940fe3182f55
Miguel Lah <xmiglah@blue-tc.com>
2013-02-13 10:42:54 +0100

    Updating src/test.c

--- src/test.c
+++ src/test.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include "test.h"
 
+/* This is just a test app for the GIT training */
 int main()
 {
   printf("App name: %s\n", APP_NAME);
   printf("App version: %s\n", APP_VERSION);
+  printf("GIT rules!\n");
 }
